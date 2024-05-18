282488599772ee1e14da95365a55c8ede7f74135
Sarath Chandra Vidya Sagar Machupalli <vidyasagar.msc@ibm.com>
2024-05-18 11:51:02 -0500

    Support for Python 3 added

---
+++ README.md
@@ -0,0 +1,20 @@
+# w-shingle generator updated for Python 3.x
+```
+usage: w-shingle.py [-h] [-w W] string
+
+Transform a string into tokens of length w.
+
+positional arguments:
+  string      the string to tokenize
+
+optional arguments:
+  -h, --help  show this help message and exit
+  -w W        the number of words per shingle
+```
+
+### Example
+
+``` bash
+$ python w_shingle.py "a rose is a rose is a rose" -w 4
+[['a', 'rose', 'is', 'a'], ['rose', 'is', 'a', 'rose'], ['is', 'a', 'rose', 'is'], ['a', 'rose', 'is', 'a'], ['rose', 'is', 'a', 'rose']]
+```
---
+++ w_shingle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+import argparse
+
+
+def main(argv):
+    # Define arguments
+    parser = argparse.ArgumentParser(description="Transform a \
+    string into tokens of length w.")
+    parser.add_argument("string", type=str, help="the string to \
+    tokenize")
+    parser.add_argument("-w", type=int, default=3, help="the \
+    number of words per shingle")
+
+    args = parser.parse_args(argv[1:])
+
+    try:
+        print(w_shingle(args.string, args.w))
+    except Exception as err:
+        print(err)
+        parser.print_help()
+
+
+def w_shingle(string, w):
+    """Return the set of contiguous sequences (shingles) of `w` words
+    in `string`."""
+    words = string.split()
+    num_words = len(words)
+
+    # Confirm that 0 < `w` <= `num_words`
+    if w > num_words or w == 0:
+        raise Exception('Invalid argument -w')
+
+    # If w is equal to the number of words in the input string, the
+    # only item in the set is `words`.
+    return [words[i:i + w] for i in range(len(words) - w + 1)]
+
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv)
